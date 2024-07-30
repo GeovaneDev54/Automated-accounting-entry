b1597ffd0884560647628cc96f7d4fc90836d331
GeovaneDev54 <geovanethecoder@gmail.com>
2024-07-30 18:18:40 -0300

    finished file

--- assets/utils/file.py
+++ assets/utils/file.py
@@ -2,4 +2,16 @@ import openpyxl
 
 class File:
     def __init__(self, file:str):
-        pass
+        self.products = {}
+        self.workbook = openpyxl.load_workbook(file)
+        self.sheet_products = self.workbook['Produtos']
+
+    def get_products(self):
+        for row in self.sheet_products.iter_rows(min_row=2):
+            product = []
+
+            for column in range(0, 17):
+                product.append(row[column].value)
+                self.products[column] = product
+
+        return self.products
